834ccdc6ecd87f9a058c50b4811ab8eca5abee2f
roldanm02 <roldan.m@northeastern.edu>
2024-01-02 13:50:45 -0500

    Added bounds to address lookup for more refined results

--- backend/address.py
+++ backend/address.py
@@ -1,4 +1,4 @@
-from backend.constants import GOOGLE_CLIENT
+from backend.constants import GOOGLE_CLIENT, LOOKUP_BOUNDS
 
 # address_to_Place_ID:
 #   Given a human readable address, retrieve Place ID using Geocoding API
@@ -11,6 +11,17 @@ def address_to_place_id(address:str) -> str:
     :rtype: string place_id from Geocoding API
     '''
 
-    geocode_result = GOOGLE_CLIENT.geocode(address=address)
+    geocode_result = GOOGLE_CLIENT.geocode(address=address, bounds=LOOKUP_BOUNDS)
     place_id = 'place_id:' + geocode_result[0].get('place_id')
     return place_id
+
+def latlng_to_address(latlng:dict) -> str:
+
+    '''
+    Convert lattitude and longitude (latlng) coordinates to human readable address
+    using Geocoding API.
+    :param latlng: dict encoding of lat, long coordinates
+    :rtype: address as a string
+    '''
+    address_result = GOOGLE_CLIENT.reverse_geocode(latlng=latlng)
+    return address_result[0]['formatted_address']
--- backend/constants.py
+++ backend/constants.py
@@ -11,3 +11,8 @@ TRAVEL_MODE = Enum('travel_mode', ['driving', 'bicycling', 'transit', 'walking']
 
 # Client for calls to Google Maps APIs
 GOOGLE_CLIENT = googlemaps.Client(key=GOOGLE_KEY)
+
+# Bounds constant for more precise address lookup
+northeast = '43.146489,-69.681923'
+southwest = '41.2131168,-72.258425'
+LOOKUP_BOUNDS = {'northeast': northeast, 'southwest': southwest}
