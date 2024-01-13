a635850ab60c8de40f58bb24dc1fbf78c85299f2
roldanm02 <roldan.m@northeastern.edu>
2024-01-13 16:00:50 -0500

    added descriptor for bicre_api top level function

--- backend/bicre_api.py
+++ backend/bicre_api.py
@@ -5,6 +5,14 @@ from backend.route_evaluation import evaluate_route
 
 # Top level function:
 def execute_bicre(start_addr:str, end_addr:str) -> list:
+    '''
+    Takes in two addresses to search for as a start and end point to the integrated route.
+    Returns a list of steps needed to get from the start address to the end address through cycling and transit.
+
+    :param start_addr: start address of the desired route
+    :param end_addr: end address of the desired route
+    :rtype list: list of dicts where each dict is a step containing information about that step in the route
+    '''
     start_pid = address_to_place_id(start_addr)
     end_pid = address_to_place_id(end_addr)
 
